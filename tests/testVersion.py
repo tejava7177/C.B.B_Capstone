d9099046d4204bffd8372f4e65a55e5053d0f840
Sim-JuHeun <juheun9912@gmail.com>
2025-01-25 14:23:46 +0900

    [feat] 음원 생성 및 필요 패키지 설치

---
+++ tests/testVersion.py
@@ -0,0 +1,11 @@
+import librosa
+import soundfile
+import numpy
+import scipy
+from pydub import AudioSegment
+
+print("librosa version:", librosa.__version__)
+print("soundfile version:", soundfile.__version__)
+print("numpy version:", numpy.__version__)
+print("scipy version:", scipy.__version__)
+print("pydub imported successfully!")
