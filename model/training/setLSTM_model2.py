e141064b70c4c4d7c67168e5e659f79527b7e5c1
Sim-JuHeun <juheun9912@gmail.com>
2025-02-21 13:57:35 +0900

    model3 생성

--- model/training/setLSTM_model2.py
+++ model/training/setLSTM_model2.py
@@ -7,17 +7,20 @@ from tensorflow.keras.utils import to_categorical
 # 데이터 로드
 X = np.load("/Users/simjuheun/Desktop/개인프로젝트/C.B.B/model/dataset/X.npy")
 Y = np.load("/Users/simjuheun/Desktop/개인프로젝트/C.B.B/model/dataset/Y.npy")
-NUM_CLASSES = Y.shape[1]  # 🔥 43개 코드 반영
+
+# 하이퍼파라미터 설정
+SEQUENCE_LENGTH = X.shape[1]  # 자동 설정 (현재 3)
+NUM_CLASSES = Y.shape[1]  # 고유 코드 개수 (현재 42개)
 
 # 모델 하이퍼파라미터
-EMBEDDING_DIM = 16
-LSTM_UNITS = 64
-BATCH_SIZE = 64
+EMBEDDING_DIM = 32
+LSTM_UNITS = 128
+BATCH_SIZE = 128
 EPOCHS = 50
 
 # LSTM 모델 생성
 model = Sequential([
-    Embedding(input_dim=NUM_CLASSES, output_dim=EMBEDDING_DIM, input_length=X.shape[1]),
+    Embedding(input_dim=NUM_CLASSES, output_dim=EMBEDDING_DIM, input_length=SEQUENCE_LENGTH),
     LSTM(LSTM_UNITS, return_sequences=True),
     LSTM(LSTM_UNITS),
     Dense(64, activation='relu'),
@@ -30,7 +33,7 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 # 모델 학습
 history = model.fit(X, Y, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_split=0.1)
 
-# 모델 저장
-model.save("/Users/simjuheun/Desktop/개인프로젝트/C.B.B/model/training/lstm_chord_model2.h5")
+# 학습된 모델 저장
+model.save("/Users/simjuheun/Desktop/개인프로젝트/C.B.B/model/training/lstm_chord_model3.h5")
 
-print("✅ 새롭게 학습된 LSTM 모델 저장 완료!")
+print("✅ 새로운 LSTM 모델 학습 완료 및 저장 완료!")
